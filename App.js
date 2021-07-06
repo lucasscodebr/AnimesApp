65fab96622b4f6a70dcf5c69514f79cc24a64f3b
lucasscode <lucass.code@gmail.com>
2021-07-05 21:56:09 -0300

    Update Version

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import {LogBox} from 'react-native';
+import React from 'react'
 import ViewMain from './src/views/index.js'
+
 const App = () => {
-  // LogBox.ignoreAllLogs()
-  // LogBox.ignoreLogs([
-  //             // 'Encountered two children with the same key',
-  //             'RCTBridge required dispatch_sync to load RCTDevLoadingView'
-  //           ])
-  return (<ViewMain />);
-};
+    // LogBox.ignoreAllLogs()
+    // LogBox.ignoreLogs([
+    //             // 'Encountered two children with the same key',
+    //             'RCTBridge required dispatch_sync to load RCTDevLoadingView'
+    //           ])
 
+    return <ViewMain />
+}
 
-export default App;
+export default App

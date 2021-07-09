5f3ec21d89b43f936b5ba1d0e164ca87166b2f29
lucasscode <lucass.code@gmail.com>
2021-07-09 19:13:22 -0300

    refactor: export components

--- src/components/index.js
+++ src/components/index.js
@@ -6,12 +6,3 @@ export * from './MiniCard'
 export * from './Episode'
 export * from './ArrowBack'
 export * from './RenderCard'
-
-//export const Video = VideoComponent
-// export const Card = CardComponent
-// export const Header = HeaderComponent
-// export const Search = SearchComponent
-// export const MiniCard = MiniCardComponent
-// export const Episode = EpisodeComponent
-// export const ArrowBack = ArrowBackComponent
-// export const RenderCard = RenderCardComponent

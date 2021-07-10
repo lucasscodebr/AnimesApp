62759e1fd26196d5d293700b00be4107ce978b13
lucasscode <lucass.code@gmail.com>
2021-07-09 23:00:00 -0300

    refactor: move Sut to a factory method

--- src/domain/data/usecases/category/remote-category.spec.ts
+++ src/domain/data/usecases/category/remote-category.spec.ts
@@ -1,11 +1,20 @@
 import {HttpGetClientSpy} from '../../test/mock-http-client'
 import {RemoteCategory} from './remote-category'
 
+type SutTypes = {
+    sut: RemoteCategory
+    httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url: string = 'any_thing'): SutTypes => {
+    const httpGetClientSpy = new HttpGetClientSpy()
+    const sut = new RemoteCategory(url, httpGetClientSpy)
+    return {sut, httpGetClientSpy}
+}
 describe('RemoteCategory', () => {
     test('Should call HttpClient with correct URL', async () => {
         const url = 'any_thing'
-        const httpGetClientSpy = new HttpGetClientSpy()
-        const sut = new RemoteCategory(url, httpGetClientSpy)
+        const {sut, httpGetClientSpy} = makeSut(url)
         await sut.handleGetCategoreis()
         expect(httpGetClientSpy.url).toBe(url)
     })

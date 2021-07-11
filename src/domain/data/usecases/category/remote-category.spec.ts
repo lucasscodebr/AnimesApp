f29be9296890ae42aec35377a213ef5a9a175cee
lucasscode <lucass.code@gmail.com>
2021-07-11 11:06:07 -0300

    test: find category by id

--- src/domain/data/usecases/category/remote-category.spec.ts
+++ src/domain/data/usecases/category/remote-category.spec.ts
@@ -1,12 +1,13 @@
 import {HttpGetClientSpy} from '../../test/mock-http-client'
 import {RemoteCategory} from './remote-category'
+import faker from 'faker'
 
 type SutTypes = {
     sut: RemoteCategory
     httpGetClientSpy: HttpGetClientSpy
 }
 
-const makeSut = (url: string = 'any_thing'): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
     const httpGetClientSpy = new HttpGetClientSpy()
     const sut = new RemoteCategory(url, httpGetClientSpy)
     return {sut, httpGetClientSpy}
@@ -14,9 +15,17 @@ const makeSut = (url: string = 'any_thing'): SutTypes => {
 
 describe('RemoteCategory', () => {
     test('Should call HttpClient with correct URL', async () => {
-        const url = 'any_thing'
+        const url = faker.internet.url()
         const {sut, httpGetClientSpy} = makeSut(url)
         await sut.handleGetCategoreis()
         expect(httpGetClientSpy.url).toBe(url)
+    })
+
+    test('Should call HttpClient with correct ID', async () => {
+        const url = faker.internet.url()
+        const id = 'any_id'
+        const {sut, httpGetClientSpy} = makeSut(url)
+        await sut.handleGetCategoryById(id)
+        expect(httpGetClientSpy.id).toBe(id)
     })
 })
